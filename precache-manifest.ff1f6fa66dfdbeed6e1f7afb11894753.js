364395d82fcf74bd9a3a120f70006455d612767f
MD SAQLAN NASIM <48436043+saqlain05@users.noreply.github.com>
2020-09-29 14:11:57 +0530

    Add files via upload

---
+++ precache-manifest.ff1f6fa66dfdbeed6e1f7afb11894753.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ae398ac55a95a5cd6825add323e1bc7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c0505c169c62f18eff23",
+    "url": "/static/css/main.cb78602d.chunk.css"
+  },
+  {
+    "revision": "9dfcec736a5c67960ba6",
+    "url": "/static/js/2.8e707828.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8e707828.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0505c169c62f18eff23",
+    "url": "/static/js/main.9393a0cc.chunk.js"
+  },
+  {
+    "revision": "96571183a6fc47f7f935",
+    "url": "/static/js/runtime-main.be526e30.js"
+  }
+]);
